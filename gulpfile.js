df908bba9cf7344f023d10e7ff3ec04baffc60e2
Alex Ivantsov <Lexin1n3@gmail.com>
2022-03-30 23:23:52 +0500

    scss to css

--- gulpfile.js
+++ gulpfile.js
@@ -1,35 +1,64 @@
-var gulp = require('gulp'),
-	autoprefixer = require('gulp-autoprefixer'),
-	sass = require('gulp-sass'),
-	sourcemaps =require('gulp-sourcemaps'),
-	browserSync = require('browser-sync').create();
+// var gulp = require('gulp'),
+// // sass = require('gulp-sass');
+// sass = require('gulp-sass')(require('sass'));
+
+// sass.compiler = require('node-sass');
+
+
+// gulp.task('hello', function (done) {
+// console.log('Hello World!');
+// done();
+// });
+
+// gulp.task('sass', function () {
+// return gulp.src('./scss/**/*.scss')
+// .pipe(sass().on('error', sass.logError))
+// .pipe(gulp.dest('./css'));
+// });
+
+// var gulp = require('gulp'),
+// sass = require('gulp-sass')(require('sass'));
+
+// sass.compiler = require('node-sass');
+
+// gulp.task('hello', function (done) {
+// console.log('Hello World!');
+// done();
+// });
+
+// gulp.task('sass', function () {
+// return gulp.src('./scss/**/*.scss')
+// .pipe(sass().on('error', sass.logError))
+// .pipe(gulp.dest('./css'));
+// });
+
+// gulp.task('watch', function () {
+// gulp.watch('./scss/**/*.scss', gulp.series('sass'));
+// })
+
+// var gulp = require('gulp'),
+// sass = require('gulp-sass');
+
+// sass.compiler = require('node-sass');
+
+// gulp.task('hello', function (done) {
+// console.log('Hello World!');
+// done();
+// });
+
+var gulp = require('gulp');
+const sass = require('gulp-sass')(require('sass'));
+// sass = require('gulp-sass');
 
 sass.compiler = require('node-sass');
 
 gulp.task('hello', function (done) {
-	console.log('Hello World!');
-	done();
+console.log('Hello World!');
+done();
 });
 
 gulp.task('sass', function () {
-	return gulp.src('./scss/style.scss')
-	.pipe(sourcemaps.init())
-	.pipe(sass().on('error', sass.logError))
-	.pipe(autoprefixer({
-			cascade: false
-	}))
-	.pipe(sourcemaps.write())
-	.pipe(gulp.dest('./css'))
-	.pipe(browserSync.stream());
+return gulp.src('./scss/**/*.scss')
+.pipe(sass().on('error', sass.logError))
+.pipe(gulp.dest('./css'));
 });
-
-gulp.task('watch', function () {
-	browserSync.init({
-        server: {
-            baseDir: "./"
-        },
-        port: 3000
-    });
-	gulp.watch('./scss/style.scss', gulp.series('sass'));
-	gulp.watch('./**/*.html').on('change', browserSync.reload);
-})
